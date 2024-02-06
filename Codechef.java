1b68102716a14d7a1d7b7cab9e6e4d3eed6738c5
nikhilkumar7488 <148653974+nikhilkumar7488@users.noreply.github.com>
2024-02-06 23:20:03 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Codechef {
+    public static void main(String[] args) {
+        // int num = 2;
+        Scanner read = new Scanner(System.in);
+        int num = read.nextInt();
+        System.out.println(num * num);
+    }
+}
